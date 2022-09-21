5972b8e12e1aafe1daeea4d99bbede55799c303b
Your Name <you@example.com>
2022-09-21 08:47:57 -0500

    evidencia

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BootcampController;
+use App\Http\Controllers\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,4 @@ Route::get('prueba', function(){
 
 //Vincular el controlador bootcamp a sus respecgtivas rutas
 Route::apiResource('bootcamps', BootcampController::class);
+Route::apiResource('courses', CourseController::class);
